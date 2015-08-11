cad5394f3717bc62652f89bfa784e3585b865a09
justboh <justin.bohnet@gmail.com>
2015-08-11 12:12:15 -0600

    function for easily saving data

--- tools/plot_style.py
+++ tools/plot_style.py
@@ -16,3 +16,23 @@ cyan = '#56B4E9'
 aqua = '#009E73'
 yellow = '#F0E442'
 navy = '#002b36'
+
+def save_data_txt(filename, out_list, col_names=False):
+    """
+    filename: string with filename, including extenstion
+    out_array: list with different data to be output
+    col_names: string with comma seperated column names
+    """
+    cn = col_names + " \n"
+    fh = open(filename, 'w+')    
+    if col_names is not False:
+        fh.write(cn)
+
+    s=''
+    out = map(list, zip(*out_list))
+    for row in out:
+        for i in row:
+            s = s+'%f,'% (i)
+        s = s+" \n"
+    fh.write(s)
+    fh.close()
