02e434a2833aef338f4543b0afbad3e0ee96fea6
justboh <justin.bohnet@gmail.com>
2015-09-09 09:25:17 -0600

    !! Update decay 8/11 squeeze

--- paper_figs/penning_squeeze/SE_squeeze_Batch_8_11_fig.py
+++ paper_figs/penning_squeeze/SE_squeeze_Batch_8_11_fig.py
@@ -120,10 +120,9 @@ plt.legend(loc=0,fontsize=10)
 
 #________________________________________________________________________
 #add some theory curves
-mult = 2
-G_el = mult* 60.56
-G_ud = mult* 9.075
-G_du = mult* 6.413
+G_el =  57.1
+G_ud =  8.56
+G_du =  6.05
 
 psi = np.linspace(0.001,pi,num=100) # radians
 
