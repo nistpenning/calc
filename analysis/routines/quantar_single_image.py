ea62ff5f8f3714ae48bd39467c4486ebb5057bd6
justboh <justin.bohnet@gmail.com>
2015-10-01 20:01:50 -0600

    10/1 quantar image

--- analysis/routines/quantar_single_image.py
+++ analysis/routines/quantar_single_image.py
@@ -16,33 +16,36 @@ importlib.reload(quantar_image)
 from skimage.feature import peak_local_max
 
 #%%
-x0 = 53.
-y0 = -16.25
-num_img = 25
-wrot = 188e3
-data_num = 1925
+x0 = 42.5
+y0 = -21.5
+num_img = 26
+wrot = 185e3
+data_num = 9380
 
 fdir = os.getcwd()
 
 qi = quantar_image.QuantarImage(x0=x0,y0=y0,fwall=wrot)
 xyt = qi.read_file_range(fdir, data_num, num_img)
 xytr = qi.rot_frame(xyt)
-xyt_bg = qi.read_file_range(fdir, 1794, num_img)
+xyt_bg = qi.read_file_range(fdir, 9540, num_img)
 xytr_bg = qi.rot_frame(xyt_bg)
 
 #%%
 #img = qi.make_image(xytr, gfilter=0.2,
               #im_range=quantar_image.im_extent(100), cmap=quantar_image.bluehot_cmap)
 img = qi.make_image(xytr, gfilter=0.2,im_range=quantar_image.im_extent(110))
-img = qi.make_image(xytr, bck=xytr_bg, gfilter=0.2,
-                    im_range=quantar_image.im_extent(110),
-                    int_range=[0.0,60.0],
-                    cmap=quantar_image.bluehot_cmap)
+if False:
+    img = qi.make_image(xytr, bck=xytr_bg, gfilter=0.2,
+                        im_range=quantar_image.im_extent(110),
+                        int_range=[0.0,60.0],
+                        cmap=quantar_image.bluehot_cmap)
 
 print("x0: {}, y0: {}, File#: {}".format(x0,y0,data_num))
 
-img_rescale = qi.fix_contrast(img, debug=True)
-              
+#img_rescale = qi.fix_contrast(img, debug=True)
+
+           
 coord = qi.get_ion_positions(img, extent=[90,160,90,160], min_distance=1.5,
                              threshold_rel=0.34)
+
 print(np.shape(coord))
