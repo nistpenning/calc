1ae814b8a98c508d58d849bb3cb4c616f83a5611
justboh <justin.bohnet@gmail.com>
2015-10-25 20:33:27 -0600

    fix jackknife estimate into single function with variable block size

--- tools/resample_tools.py
+++ tools/resample_tools.py
@@ -9,40 +9,44 @@ import os, importlib, shutil
 import numpy as np
 import numpy.random as rand
 from numpy import pi, sin, cos, sqrt
+    
+def jackknife_est(array, func, block_size=1, verbose=False):
+    """
+    Returns jack_knife est of variance and 1 std err deviation of variance of an array
+    Parameters:
+        array (numpy array): Array of values to have var estimated
+        func: func that takes an array, returns a single value
+        block_size (Optional int): Block size of jackknife. default is single value
+    Returns:
+        var, std_dev_var: variance and 1 std dev of variance as estimated with jackknife.    
+    """
+    n = np.size(array)
+    nb = int(n/block_size)  #number of blocks
+    est = func(array)
 
-def jackknife_var_on_mean(array, block_size=1):
-    n = np.size(array)
-    mean = np.mean(array)
-    var = np.var(array)
-    var_mean = var/(n)
+    jack_pseudos = np.array([nb*est - (nb-1)*func(np.concatenate((array[:i],array[i+block_size:]))) for i in range(nb)])
+    jack_est = (1/float(nb))*np.sum(jack_pseudos)  
+    jack_est_var = (1/float(nb-1))*np.sum((jack_pseudos-est)**2)
+    jack_std_dev_of_est = sqrt(jack_est_var)/sqrt(nb)
+    if verbose is True:
+        print("Jackknife estimate: {:.3g}".format(jack_est))
+        print("1 sig error on Jackknife estimate: {:.3g}".format(jack_std_dev_of_var))
     
-    jack_ests = np.array([(1/(n-1))*np.sum(np.concatenate((array[:i],array[i+1:]))) for i in range(n)])
-    jack_var_mean = ((n-1)/float(n))*np.sum((jack_ests-mean)**2)
-    print("Std var mean: {:.3g}, Jackknife var mean: {:.3g}".format(var_mean,jack_var_mean))
-    
-    return jack_var_mean
-    
-def jackknife_err_on_var(array, block_size=1):
-    n = np.size(array)
-    var = np.var(array)
-    
-    jack_pseudos = np.array([n*var - (n-1)*np.var(np.concatenate((array[:i],array[i+1:]))) for i in range(n)])
-    jack_pseudo_mean = (1/float(n))*np.sum(jack_pseudos)  
-    jack_var_var = (1/float(n-1))*np.sum((jack_pseudos-jack_pseudo_mean)**2)
-    print("Std err var: {:.3g}, Jackknife err var: {:.3g}".format(sqrt(var)/sqrt(2*(n-1)),sqrt(jack_var_var)/2.0/sqrt(n)))
-    
-    return jack_pseudo_mean,jack_var_var
+    return jack_est, jack_std_dev_of_est
 
 #tests    
 if __name__=='__main__':
-    data = rand.normal(loc=0.0, scale=1.0, size=500)
-    jack_var_mean = jackknife_var_on_mean(data)  
-    frac_diff = jack_var_mean/(np.var(data)/np.size(data))
-    print(frac_diff)
-    if frac_diff>0.99 and frac_diff<1.01:
-        print("Jacknife variance test with Gaussian: OK")
+    samples = 200
+    data = rand.normal(loc=0.0, scale=2.0, size=samples)   
+    
+    jack_std_dev_of_var = jackknife_est(data, np.var, verbose=True)[1]
+    frac_diff = jack_std_dev_of_var/( np.var(data)*sqrt(2/(samples-1)) )
+    if frac_diff>0.9 and frac_diff<1.1:
+        print(frac_diff)
+        print("Standard 1 sig error on estimate: {:.3g}".format(np.var(data)*sqrt(2/(samples-1))))
+        print("Jacknife 1 std dev of var, test with Gaussian: OK")
     else:
-        print("Jacknife variance test with Gaussian: FAIL")
-        
-    jack_pseudo_mean,jack_var_var = jackknife_err_on_var(data)
-    print(jack_pseudo_mean)
+        print(frac_diff)
+        print("Standard 1 sig error on estimate: {:.3g}".format(np.var(data)*sqrt(2/(samples-1))))
+        print("Jacknife 1 std dev of var, test with Gaussian: FAIL")
+    
