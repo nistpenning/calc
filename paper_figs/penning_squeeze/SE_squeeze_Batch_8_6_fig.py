1aa7073f74395f3b463475788b6d88eec4b18b11
justboh <justin.bohnet@gmail.com>
2015-11-04 09:18:28 -0700

    update 100 ion from 8/6 fig with error analysis, correct dephasing

--- paper_figs/penning_squeeze/SE_squeeze_Batch_8_6_fig.py
+++ paper_figs/penning_squeeze/SE_squeeze_Batch_8_6_fig.py
@@ -7,35 +7,37 @@ Created on Thu Jul 23 14:42:25 2015
 
 import os, shutil
 import numpy as np
-from numpy import pi, sqrt
+from numpy import pi, sqrt, sin
 import matplotlib.pyplot as plt
 
 import hfGUIdata as hf
 import plot_style as ps
 import squeeze_func_time as squ
+from matplotlib.ticker import FixedLocator, FormatStrFormatter
 
 #options
 verbose = True
 save = True
-img_name = "spinNoise_8_6"
-folder_name = "/Volumes/688/Public/penning_britton/dailyLabBookFiles/2015/20150806/Squeeze_186kHz/"
+img_name = "spinNoise_8_6_100ions"
+folder_name = "/Users/jgb/Data/20150806/Squeeze_186kHz/"
 
 #added noise from Jy noise fit
 A = 0.001700  # rad^2/ms^2
-B = 0.00000 # rad^2/ms^4
+B = 0.00006 # rad^2/ms^4
 
 # containers for data sets
 psis=[]
 its=[]
 sig_obs = []
 sig_ins = []
+sig_2_in_err = []
 sig_pns = []
 Ns = []
 names = []
 
 base_path = os.path.normpath(folder_name)
 fns = [os.listdir(base_path)[i] for i in [0,1,2]]
-fns = [os.listdir(base_path)[i] for i in [6,5]]
+fns = [os.listdir(base_path)[i] for i in [6,1,5]]
 J1ks = (475.0*3.03)*np.ones(np.shape(fns))
 Ncals = 1.3999 * np.ones(np.shape(fns))  # #photons per ion per ms
 
@@ -71,10 +73,11 @@ for i,fn in enumerate(fns):
     psi_deg = data.T[0]
     count_avg = data.T[1]
     sig_sn = sqrt(data.T[1])
+    sig_sn_2_err = sig_sn**2 * sqrt(2/reps)
     sig_ob = data.T[2]
     sig_in = sqrt(sig_ob**2 - sig_sn**2)  # subtract poissonian shot noise
-    sig_ob_err = sig_ob * 1/sqrt(2*reps)
-    sig_in_err= sig_in * 1/sqrt(2*reps)
+    sig_ob_2_err = sig_ob**2 * sqrt(2/reps)
+    sig_in_2_err= sqrt( (sig_ob_2_err)**2  + (sig_sn_2_err)**2)
 
     sig_pn = sqrt(k**2/4.0/N)  # calclated from the atom number
 
@@ -101,6 +104,7 @@ for i,fn in enumerate(fns):
     its.append(int_t)
     sig_obs.append(sig_ob)
     sig_ins.append(sig_in)
+    sig_2_in_err.append(sig_in_2_err)
     sig_pns.append(sig_pn)
     Ns.append(N)
     names.append(hf.n_slice(file_name))
@@ -109,12 +113,16 @@ for i,fn in enumerate(fns):
 #%%
 #________________________________________________________________________
 # visualizing the experimental data
+fig, ax = plt.subplots() 
 colors = ['k', ps.blue, ps.red]
 for i,data in enumerate(sig_obs):
     l = r"$\tau=$ {:.1f} ms, N: {:.0f}".format(its[i]*1e3,Ns[i])
     spin_noise = (sig_ins[i]**2)/(sig_pns[i]**2)
+    spin_noise_err = sig_2_in_err[i]/(sig_pns[i]**2)
+    spin_noise_err = sqrt( (sig_2_in_err[i]/sig_pns[i]**2)**2 + ((sig_ins[i]/sig_pns[i])**2 * 0.05)**2 ) # accounting for 5% uncertainty in PN
     spin_noise_dB = 10*np.log10(spin_noise)
-    spin_noise_err_dB = 10*np.log10(spin_noise) - 10*np.log10(spin_noise-2*spin_noise/sqrt(2*reps))
+    #spin_noise_err_dB = 10*np.log10(spin_noise) - 10*np.log10(spin_noise-2*spin_noise/sqrt(2*reps))
+    spin_noise_err_dB = 10*np.log10(spin_noise) - 10*np.log10(spin_noise-spin_noise_err)
     plt.errorbar(np.abs(psis[i]-180),spin_noise_dB,yerr=spin_noise_err_dB, fmt='o',label=l, color=colors[i])
 
 #plt.yscale('log')
@@ -123,7 +131,12 @@ plt.axis([2,181,-9,15])
 plt.xlabel(r"Tomography angle $\psi$ (deg)")
 plt.ylabel(r"Spin variance $(\Delta S_\psi)^2$/N/4 (dB)")
 plt.grid('off')
-#plt.legend(loc=0,fontsize=10)
+plt.legend(loc=0,fontsize=10)
+
+majorLocator = FixedLocator([5,50,180])
+majorFormatter = FormatStrFormatter('%d')
+ax.xaxis.set_major_locator(majorLocator)
+ax.xaxis.set_major_formatter(majorFormatter)
 
 #________________________________________________________________________
 #add some theory curves
@@ -132,6 +145,7 @@ G_ud =  8.76
 G_du =  6.19
 #include additional decoherence term
 G_el = G_el + 45.0
+G_tot = 0.5*(G_el + (G_ud+G_du))
 
 psi = np.linspace(0.001,pi,num=500) # radians
 
@@ -141,8 +155,9 @@ for i,name in enumerate(names):
     out = squ.OAT_decoh(-psi, its[i], Jbar, Ns[i], G_el, G_ud, G_du)
     out_u = squ.OAT_decoh(-psi, its[i], Jbar, Ns[i]+5, G_el, G_ud, G_du)
     out_l = squ.OAT_decoh(-psi, its[i], Jbar, Ns[i]-5, G_el, G_ud, G_du)
+    C = np.exp(-G_tot*its[i])  # reduction in contrast from spontaneous emission
     R = np.real(out[0]/(sqrt(Ns[i])/(2.0)))**2
-    R_add = R + (A*(its[i]*1e3)**2)*N * sin(psi) + (B*(its[i]*1e3)**4)*N * sin(psi) + (22.5/((1091.0+1085.0)/2.0))**2 / (1/Ns[i])
+    R_add = R + (A*(its[i]*1e3)**2)*(N*C**2)*sin(psi)**2 + (B*(its[i]*1e3)**4)*(N*C**2)*sin(psi)**2 
     R_dB = 10*np.log10(R)
     R_dB_add = 10*np.log10(R_add)
     plt.plot(np.abs(psi*180/pi -180),R_dB,color=colors[i],linestyle='--')
