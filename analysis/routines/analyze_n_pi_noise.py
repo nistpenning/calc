be33099933b504072e16547833274808b5122f2d
justboh <justin.bohnet@gmail.com>
2015-12-04 08:46:51 -0700

    show both average and noise in n_pi_analyze

--- analysis/routines/analyze_n_pi_noise.py
+++ analysis/routines/analyze_n_pi_noise.py
@@ -17,12 +17,10 @@ import plot_model_fit as pt
 #options
 colors = ['k', ps.red, ps.blue, ps.orange, ps.pink]
 
-files_to_use = [-1] 
+files_to_use = -1
 Ncal = 1.70
 
 base_path = os.getcwd()
-fns = [os.listdir(base_path)[i] for i in files_to_use]
-fns = [i for i in fns if not i.startswith('.DS_Store')]
 
 #%%
 #_____________________________________________________________________
@@ -34,16 +32,22 @@ files = os.listdir(os.getcwd())
 
 #Load the data 
 file_name, scandata, avg_pmt_counts, pmterr, trials, data = hf.get_raw_counts()
-bm = hf.get_ionProp_value('det_brightMean')
-dm = hf.get_ionProp_value('det_darkMean')
-tpi = hf.get_ionProp_value('sf_fitParam_tpi')
-det_t = hf.get_ionProp_value('det_t')
+bm = hf.get_ionProp_value('detection%det_brightMean')
+dm = hf.get_ionProp_value('detection%det_darkMean')
+b_frac = (avg_pmt_counts-dm)/(bm-dm)
+tpi = hf.get_ionProp_value('sf%fitParams%sf_fitParam_tpi')
+det_t = hf.get_ionProp_value('detection%det_t')
 k = bm-dm  # phtns per N atoms
 N = k/(det_t*1e-3)/Ncal
 
 os.chdir(base_path)
+plt.subplot(211)
+plt.plot(scandata,b_frac,'o')
+plt.xlabel("# pi pulses")
+plt.ylabel("Bright Fraction")
+plt.axis([np.min(scandata),np.max(scandata),0,1.1])
 
-
+plt.subplot(212)
 # analysis here
 def n_pi_std_fit(n_pi, y0, A):
     sig_tot_2 = y0**2 + (A*n_pi)**2
@@ -51,9 +55,12 @@ def n_pi_std_fit(n_pi, y0, A):
 
 guess=np.array([90.0,1.0])
 hold=np.array([False,False])
-pl = ["# pi pulses","Std Dev. photon count",'uWave amplitude noise']
+a = [np.min(scandata),np.max(scandata),0,np.max(pmterr)*1.1]
+pl = ["# pi pulses","Std Dev. m (phtns)",'']
 pout,perr = pt.plot_fit(scandata,pmterr,n_pi_std_fit,guess,
             hold=hold,
-            labels=pl)
+            labels=pl,
+            axis=a)
 
+plt.tight_layout()
 plt.show()
